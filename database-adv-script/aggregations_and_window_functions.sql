bf932835dbdb530ed7741f8b0aa7639368710379
emmaedem <emmaontop86@yahoo.com>
2025-11-10 15:32:56 +0100

    modified:   database-adv-script/aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -19,3 +19,12 @@ FROM bookings
 GROUP BY property_id
 ORDER BY total_bookings DESC;
 
+-- Assign a unique row number to each property (using ROW_NUMBER)
+
+SELECT 
+    property_id,
+    COUNT(booking_id) AS total_bookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(booking_id) DESC) AS row_number
+FROM bookings
+GROUP BY property_id
+ORDER BY total_bookings DESC;
